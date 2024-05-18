9d73b025a478b3e78e8ac9e946af776a9c133289
Kawsar-Ahmed-Ridoy <kawsarahmedridoy1@gmail.com>
2024-05-18 19:23:50 +0600

    calculate complete

--- JS/script.js
+++ JS/script.js
@@ -15,6 +15,7 @@ function getId(elementId) {
     if (seatCard.includes(getElementId.id) === false && index <= 3) {
       seatLeft(index);
       seatTotal(index);
+      calculateTotal();
       getElementId.classList.add("bg-green-500");
       getElementId.classList.add("text-white");
       seatCard.push(getElementId.id);
@@ -58,7 +59,47 @@ function seatNumber(seatNum) {
         "font-normal"
       );
       elementContainer.innerHTML = `<span>${elementText.id}</span>
-    <span>Economy</span> <span>550</span>`;
+    <span>Economy</span> <span id="taka" >550</span>`;
     }
   }
 }
+let totalPrice = 0;
+function calculateTotal() {
+  
+  totalPrice += 550;
+  const totalPriceElement = document.getElementById("total-price");
+  const grandTotal = document.getElementById("grand-total")
+  const total = totalPriceElement.innerText = totalPrice;
+  grandTotal.innerText = total
+  
+}
+
+function discountTotal(){
+  const element = document.getElementById("discount-price")
+  const input = document.getElementById('input-coupon')
+  const grandTotal = document.getElementById("grand-total")
+  // const createElement = element.innerHTML
+  const inputHide = document.getElementById('input-hide')
+
+const getTotalPrice = document.getElementById("total-price");
+const getTotalPriceNum = parseInt(getTotalPrice.innerText)
+
+
+if(input.value === "Couple 20" ){
+  inputHide.classList.add("hidden")
+    const discount = getTotalPriceNum - getTotalPriceNum * 0.2
+    const discountT = getTotalPriceNum - getTotalPriceNum * 0.2 - getTotalPriceNum
+    element.innerHTML = `<p>Discount</p>
+    <p>${discountT}</p>`
+    grandTotal.innerText = discount;
+  }
+  if(input.value === "NEW15"){
+    inputHide.classList.add("hidden")
+    const discount = getTotalPriceNum - getTotalPriceNum * 0.15
+    const discountT = getTotalPriceNum - getTotalPriceNum * 0.15 - getTotalPriceNum
+    element.innerHTML = `<p>Discount</p>
+    <p>${discountT}</p>`
+    grandTotal.innerText = discount;
+  }
+}
+
