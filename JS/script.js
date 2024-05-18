f5f135fce64f5ac5ee9bdf6cef0817bdf8ee4be5
Kawsar-Ahmed-Ridoy <kawsarahmedridoy1@gmail.com>
2024-05-18 09:00:47 +0600

    show ticket number

--- JS/script.js
+++ JS/script.js
@@ -8,9 +8,10 @@ const seatCard = [];
 function getId(elementId) {
   const index = seatCard.length;
   const getElementId = document.getElementById(elementId);
-  console.log(getElementId);
+  seatNumber(getElementId)
+  console.log(getElementId.id);
   if (getElementId.id !== "try") {
-    console.log(getElementId.id.length);
+    console.log(getElementId.id);
     if (seatCard.includes(getElementId.id) === false && index <= 3 ) {
       
       seatLeft(index)
@@ -37,5 +38,23 @@ function seatTotal (){
   const element = document.getElementById('seat-total');
   const e = parseInt(element.innerText)
   const seatNumber = e + 1
-  element.innerText = seatNumber
+  element.innerText = seatNumber;
+}
+
+function seatNumber (seatNum){
+  console.log(seatNum.id);
+  const elementText = seatNum.innerText;
+  const element = document.getElementById('seat-number');
+  if(seatCard.length <= 3 && seatCard.includes(seatNum.id) === false){
+    const elementContainer = element.appendChild(document.createElement("div")) ;
+  elementContainer.classList.add("flex", "justify-between", "my-4", "gap-x-16", "text-gray-500", "font-normal")
+  elementContainer.innerHTML = `<span>${elementText}</span>
+  <span>Economy</span> <span>550</span>`
+  }
+  
+
+  // const seatElement = document.getElementById('single-seat')
+  // seatElement.innerText = elementText;
+  // element.appendChild(seatElement)
+
 }
